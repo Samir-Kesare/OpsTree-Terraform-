9c7321957b71508f05e7aec2dbc8d63316497ee9
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-27 15:49:40 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/security-group/main.tf
+++ Dev_Infra/Static_Tf/security-group/main.tf
@@ -1,1 +1,34 @@
 
+resource "aws_security_group" "salary_sg" {
+  name        = var.security_group_name
+  description = var.security_group_description
+  vpc_id      = var.vpc_id
+
+  // Inbound rule allowing HTTP traffic
+  ingress {
+    from_port   = var.http_port
+    to_port     = var.http_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Inbound rule allowing SSH traffic
+  ingress {
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // Outbound rule allowing all traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
