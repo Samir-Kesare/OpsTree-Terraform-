ec21fb5f7161bd110c872ed841db6f70aeeba667
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-27 22:30:08 +0530

    Update variable.tf

--- Dev_Infra/Security_Group_PGSQL/variable.tf
+++ Dev_Infra/Security_Group_PGSQL/variable.tf
@@ -1,1 +1,43 @@
 
+#---------------------------------Security Group ----------------------------------#
+
+variable "security_name" {
+  description     = "Name tag for the security group"
+  type            = string
+  default         = "Postgres-sg"
+}
+variable "Security_description" {
+  description     = "Description for the security group"
+  type            = string
+  default         = "Security group for Postgres"
+}
+variable "vpc_id" {
+  description     = "ID of the VPC for instances"
+  type            = string
+  default         = "vpc-0fd6d3005998fb58c"
+}
+variable "inbound_ports" {
+  description     = "List of inbound ports and protocols and cidr block"
+  type            = list(map(any))
+  default         = [
+    { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, # Management VPC Cidr Block
+    { port = 5432, protocol = "tcp", cidr_blocks = "0.0.0.0/0" }, #  Backend-sg
+  ]
+}
+
+variable "outbound_ports" {
+  description     = "List of outbound ports and protocols and Cidr block "
+  type            = list(map(any))
+  default         = [
+    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0", },
+  ]
+}
+variable "Security_group_tags" {
+  type            = map(string)
+  default         = {
+    Name          = "Postgres-SG"
+    Enviroment    = "dev"
+    Owner         = "Vikram"
+  }
+}
+
