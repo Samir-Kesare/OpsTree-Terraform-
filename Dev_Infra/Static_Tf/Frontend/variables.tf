c51c2795008c709be827fd1629e9da2fb50b65e3
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-27 17:55:15 +0530

    Update variables.tf

--- Dev_Infra/Static_Tf/Frontend/variables.tf
+++ Dev_Infra/Static_Tf/Frontend/variables.tf
@@ -1,1 +1,47 @@
 
+#---------------------------------Security Group ----------------------------------#
+
+variable "security_name" {
+  description     = "Name tag for the security group"
+  type            = string
+  default         = "Frontend-sg"
+}
+variable "Security_description" {
+  description     = "Description for the security group"
+  type            = string
+  default         = "Security group for Frontend-API"
+}
+variable "vpc_id" {
+  description     = "ID of the VPC for instances"
+  type            = string
+  default         = "vpc-0383f4dc3af051efa"
+}
+variable "inbound_ports" {
+  description     = "List of inbound ports and protocols and cidr block"
+  type            = list(map(any))
+  default         = [
+    { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, # Management VPC Cidr Block
+    { port = 3000, protocol = "tcp", cidr_blocks = "0.0.0.0/0" }, #  Frontend-lb-sg ID Repalce (0.0.0.0/0)
+  ]
+}
+
+variable "outbound_ports" {
+  description     = "List of outbound ports and protocols and Cidr block "
+  type            = list(map(any))
+  default         = [
+    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0", },
+  ]
+}
+variable "Sg_tags" {
+  type            = map(string)
+  default         = {
+    Name          = "Frontend-sg"
+    Enviroment    = "dev"
+    Owner         = "Vishal"
+  }
+}
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
+
+
+
+
