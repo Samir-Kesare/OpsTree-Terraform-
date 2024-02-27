13fbc1c777cf31549a48e247f7d359e50498a7b5
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-27 17:56:20 +0530

    Update provider.tf

--- Dev_Infra/Static_Tf/Frontend/provider.tf
+++ Dev_Infra/Static_Tf/Frontend/provider.tf
@@ -1,1 +1,14 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.38.0"  # Using a minimum version constraint
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-northeast-1"  
+} 
