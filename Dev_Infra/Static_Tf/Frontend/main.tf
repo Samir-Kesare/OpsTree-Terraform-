6975cc09d6e01c8d962e545bbff93102305ddcc6
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-02-27 17:54:50 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/Frontend/main.tf
+++ Dev_Infra/Static_Tf/Frontend/main.tf
@@ -1,1 +1,33 @@
 
+#---------------------------------Security Group ----------------------------------#
+
+locals {
+  inbound_ports         = var.inbound_ports
+  outbound_ports        = var.outbound_ports
+}
+resource "aws_security_group" "sq_grp" {
+  name                  = var.security_name
+  description           = var.Security_description
+  vpc_id                = var.vpc_id
+
+  dynamic "ingress" {
+  for_each              = local.inbound_ports
+  content {
+   from_port            = ingress.value.port
+   to_port              = ingress.value.port
+   protocol             = ingress.value.protocol
+   cidr_blocks          = [ingress.value.cidr_blocks]
+  }
+ }
+ dynamic "egress" {
+  for_each              = local.outbound_ports
+  content {
+   from_port            = egress.value.port
+   to_port              = egress.value.port
+   protocol             = egress.value.protocol
+   cidr_blocks          = [egress.value.cidr_blocks]
+  }
+ }
+  tags                  = var.Sg_tags
+}
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
