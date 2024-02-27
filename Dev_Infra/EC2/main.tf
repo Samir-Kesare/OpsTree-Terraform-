1f10026406ada5e062e6dccb732ddb442b0e4c5d
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-27 23:53:28 +0530

    Update main.tf

--- Dev_Infra/EC2/main.tf
+++ Dev_Infra/EC2/main.tf
@@ -1,1 +1,17 @@
+#---------------------------------EC2 Instance ----------------------------------#
 
+
+resource "aws_instance" "Postgres_Instance" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [var.security_name]
+
+
+root_block_device {
+    volume_size = 16
+  }
+  
+ tags                  = var.Postgres_Instance_tag
+  
+  }
