e46cc7912383599746fcf770efd385e98cd4fce0
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-27 18:21:13 +0530

    Update variables.tf

--- Dev_Infra/Static_Tf/security-group/variables.tf
+++ Dev_Infra/Static_Tf/security-group/variables.tf
@@ -1,29 +1,43 @@
-variable "region" {
-  description = "The AWS region to deploy resources in"
-  default     = "us-east-1"
-}
 
-variable "security_group_name" {
-  description = "Name of the security group"
-  default     = "salary-sg"
-}
+#---------------------------------Security Group ----------------------------------#
 
-variable "security_group_description" {
-  description = "Description for the security group"
-  default     = "Security group for Salary application"
+variable "security_name" {
+  description     = "Name tag for the security group"
+  type            = string
+  default         = "Frontend-sg"
 }
-
+variable "Security_description" {
+  description     = "Description for the security group"
+  type            = string
+  default         = "Security group for Frontend-API"
+}
 variable "vpc_id" {
-  description = "ID of the VPC"
-  default     = "vpc-0ebc6865d6c6a5460"
+  description     = "ID of the VPC for instances"
+  type            = string
+  default         = "vpc-0383f4dc3af051efa"
+}
+variable "inbound_ports" {
+  description     = "List of inbound ports and protocols and cidr block"
+  type            = list(map(any))
+  default         = [
+    { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, # Management VPC Cidr Block
+    { port = 3000, protocol = "tcp", cidr_blocks = "0.0.0.0/0" }, #  Frontend-lb-sg ID Repalce (0.0.0.0/0)
+  ]
 }
 
-variable "http_port" {
-  description = "Port for HTTP traffic"
-  default     = 8080
+variable "outbound_ports" {
+  description     = "List of outbound ports and protocols and Cidr block "
+  type            = list(map(any))
+  default         = [
+    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0", },
+  ]
 }
-
-variable "ssh_port" {
-  description = "Port for SSH traffic"
-  default     = 22
+variable "Sg_tags" {
+  type            = map(string)
+  default         = {
+    Name          = "Frontend-sg"
+    Enviroment    = "dev"
+    Owner         = "Vishal"
+  }
 }
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
