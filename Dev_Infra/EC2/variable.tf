c30c702cd5afccf7622f49c77f308702a65f2920
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-27 23:55:06 +0530

    Update variable.tf

--- Dev_Infra/EC2/variable.tf
+++ Dev_Infra/EC2/variable.tf
@@ -1,1 +1,29 @@
+#---------------------------------EC2 Instance ----------------------------------#
 
+
+
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources"
+}
+
+variable "instance_ami" {
+  description = "The AMI for the EC2 instance"
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance"
+}
+
+variable "key_name" {
+  description = "The SSH key pair name to use for the instance"
+}
+
+variable "Postgres_Instance_tag" {
+  type            = map(string)
+  default         = {
+    Name          = "Postgres_Instance"
+    Enviroment    = "dev"
+    Owner         = "Vikram"
+  }
+}
