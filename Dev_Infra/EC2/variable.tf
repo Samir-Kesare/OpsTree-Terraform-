1c2135e99d977b45fab79c7de1bb544c89ae6745
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-02-28 13:21:46 +0530

    Update variable.tf

--- Dev_Infra/EC2/variable.tf
+++ Dev_Infra/EC2/variable.tf
@@ -1,3 +1,40 @@
+
+#---------------------------------Security Group ----------------------------------#
+
+variable "security_name" {
+  description     = "Name tag for the security group"
+  type            = string
+  default         = "Postgres_sg"
+}
+variable "Security_description" {
+  description     = "Description for the security group"
+  type            = string
+  default         = "Security group for Postgres"
+}
+
+variable "inbound_ports" {
+  description     = "List of inbound ports and protocols and cidr block"
+  type            = list(map(any))
+  
+}
+
+variable "outbound_ports" {
+  description     = "List of outbound ports and protocols and Cidr block "
+  type            = list(map(any))
+ 
+}
+variable "Security_group_tags" {
+  type            = map(string)
+  default         = {
+    Name          = "Postgres-SG"
+    Enviroment    = "dev"
+    Owner         = "Vikram"
+  }
+}
+
+
+
+
 #---------------------------------EC2 Instance ----------------------------------#
 
 
@@ -19,11 +56,40 @@ variable "key_name" {
   description = "The SSH key pair name to use for the instance"
 }
 
+variable "vpc_name" {
+  description = "Name for the VPC"
+  type        = string
+}
+
+
+variable "vpc_id" {
+  description     = "ID of the VPC for instances"
+  type            = string
+}
+
+variable "private_subnet_cidr_block" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability zone for the private subnet"
+  type        = string
+}
+
+variable "private_subnet_name" {
+  description = "Name for the private subnet"
+  type        = string
+}
+
+
 variable "Postgres_Instance_tag" {
   type            = map(string)
-  default         = {
-    Name          = "Postgres_Instance"
-    Enviroment    = "dev"
-    Owner         = "Vikram"
-  }
+  
 }
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
