44bff8ceea0e10dfc0de9adc73a108d0765573b6
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-02-28 12:44:27 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/network/main.tf
+++ Dev_Infra/Static_Tf/network/main.tf
@@ -1,6 +1,6 @@
 /*--------------- VPC ---------------*/
 
-resource "aws_vpc" "vpc-01" {
+resource "aws_vpc" "dev-vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = var.vpc_enable_dns_support
   enable_dns_hostnames = var.vpc_enable_dns_hostnames
