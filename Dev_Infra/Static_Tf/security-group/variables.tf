6fe6e3f8d9399d82969fd2a7d1f96e18cc22091b
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-28 11:44:15 +0530

    Update variables.tf

--- Dev_Infra/Static_Tf/security-group/variables.tf
+++ Dev_Infra/Static_Tf/security-group/variables.tf
@@ -1,42 +1,66 @@
-
-
-variable "security_name" {
-  description     = "Name tag for the security group"
-  type            = string
-  default         = "Salary-sg"
+variable "security_group_name" {
+  description = "Name of the security group"
+  default     = "salary-sg"
 }
-variable "Security_description" {
-  description     = "Description for the security group"
-  type            = string
-  default         = "Security group for Salary-API"
+
+variable "security_group_description" {
+  description = "Description for the security group"
+  default     = "Security group for Salary application"
 }
+
 variable "vpc_id" {
-  description     = "ID of the VPC for instances"
-  type            = string
-  default         = "vpc-0ebc6865d6c6a5460"
+  description = "ID of the VPC"
+  default     = "vpc-0ebc6865d6c6a5460"
 }
-variable "inbound_ports" {
-  description     = "List of inbound ports and protocols and cidr block"
-  type            = list(map(any))
-  default         = [
-    { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, # Management VPC Cidr Block
-    { port = 3000, protocol = "tcp", cidr_blocks = "0.0.0.0/0" }, #  Salary-lb-sg ID Repalce (0.0.0.0/0)
+
+variable "http_port" {
+  description = "Port for HTTP traffic"
+  default     = 8080
+}
+
+variable "ssh_port" {
+  description = "Port for SSH traffic"
+  default     = 22
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   ]
 }
 
-variable "outbound_ports" {
-  description     = "List of outbound ports and protocols and Cidr block "
-  type            = list(map(any))
-  default         = [
-    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0", },
+variable "egress_rules" {
+  description = "List of egress rules"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   ]
 }
-variable "Sg_tags" {
-  type            = map(string)
-  default         = {
-    Name          = "Salary-sg"
-    Enviroment    = "dev"
-    Owner         = "Shikha"
-  }
-}
-
