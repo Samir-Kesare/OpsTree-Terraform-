2dc58dd914fdf98b7e80d926e4812b3ccaffd1ae
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-28 11:41:54 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/security-group/main.tf
+++ Dev_Infra/Static_Tf/security-group/main.tf
@@ -1,31 +1,31 @@
-
-
-locals {
-  inbound_ports         = var.inbound_ports
-  outbound_ports        = var.outbound_ports
-}
-resource "aws_security_group" "sq_grp" {
-  name                  = var.security_name
-  description           = var.Security_description
-  vpc_id                = var.vpc_id
+// Create AWS security group
+resource "aws_security_group" "salary_sg" {
+  name        = var.security_group_name
+  description = var.security_group_description
+  vpc_id      = var.vpc_id
 
   dynamic "ingress" {
-  for_each              = local.inbound_ports
-  content {
-   from_port            = ingress.value.port
-   to_port              = ingress.value.port
-   protocol             = ingress.value.protocol
-   cidr_blocks          = [ingress.value.cidr_blocks]
+    for_each = var.ingress_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
   }
- }
- dynamic "egress" {
-  for_each              = local.outbound_ports
-  content {
-   from_port            = egress.value.port
-   to_port              = egress.value.port
-   protocol             = egress.value.protocol
-   cidr_blocks          = [egress.value.cidr_blocks]
+
+  dynamic "egress" {
+    for_each = var.egress_rules
+    content {
+      from_port   = egress.value.from_port
+      to_port     = egress.value.to_port
+      protocol    = egress.value.protocol
+      cidr_blocks = egress.value.cidr_blocks
+    }
   }
- }
-  tags                  = var.Sg_tags
+
+  // Tags for the security group
+  tags = {
+    Name = var.security_group_name
+  }
 }
