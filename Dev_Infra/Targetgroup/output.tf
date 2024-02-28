b76dc7616495c532a050b9ae2791e393b7b6eecb
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-28 11:59:55 +0530

    Update output.tf

--- Dev_Infra/Targetgroup/output.tf
+++ Dev_Infra/Targetgroup/output.tf
@@ -1,3 +1,15 @@
+*------------------------------------------------------------------------------------------*
+// Output for the created AWS security group
+output "security_group_id" {
+  value = aws_security_group.salary_sg.id
+}
+*------------------------------------------------------------------------------------------*
+//Launch-Template
+output "launch_template_id" {
+  value = aws_launch_template.salary_launch_template.id
+}
+*-------------------------------------------------------------------------------------------------*
+//Target-Group
 output "target_group_id" {
   value = aws_lb_target_group.salary_target_group.id
 }
