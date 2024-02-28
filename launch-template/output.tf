226dada98472815a451ee1646236b9cd2ace13c1
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-28 11:53:19 +0530

    Update output.tf

--- launch-template/output.tf
+++ launch-template/output.tf
@@ -4,7 +4,7 @@ output "security_group_id" {
   value = aws_security_group.salary_sg.id
 }
 *------------------------------------------------------------------------------------------*
-
+//Launch-Template
 output "launch_template_id" {
   value = aws_launch_template.salary_launch_template.id
 }
