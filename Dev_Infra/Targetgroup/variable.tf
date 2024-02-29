f78dab64170b31eb839cc704904c6e354a1d0063
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-02-29 13:09:18 +0530

    Update variable.tf

--- Dev_Infra/Targetgroup/variable.tf
+++ Dev_Infra/Targetgroup/variable.tf
@@ -66,6 +66,14 @@ variable "egress_rules" {
     }
   ]
 }
+variable "Sg_tags" {
+  type            = map(string)
+  default         = {
+    Name          = "salary-sg"
+    Enviroment    = "dev"
+    Owner         = "Shikha"
+  }
+}
 
 *----------------------------------------------------------------------------------------------------*
 //Launch-Template
