ef0fe2c0c17678bd03c1d059f1553c4434889b17
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-03-01 11:33:53 +0530

    Update output.tf

--- Dev_Infra/Static_Tf/network/output.tf
+++ Dev_Infra/Static_Tf/network/output.tf
@@ -1,14 +1,14 @@
 output "vpc-id" {
-  value = aws_vpc.vpc-01.id
+  value = aws_vpc.dev_vpc.id
 }
 
 output "public-subnets-id" {
-  value = aws_subnet.public_subnets.*.id
+  value = aws_subnet.dev_public_subnets.*.id
 }
 
 
 output "private-subnets-id" {
-  value = aws_subnet.private_subnets.*.id
+  value = aws_subnet.dev_private_subnets.*.id
 }
 
 output "dev-igw-id" {
