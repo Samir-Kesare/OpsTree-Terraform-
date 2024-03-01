70295f5638d0c3b0e64489809b70e2303773214c
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-03-01 10:09:52 +0530

    Update main.tf

--- launch-template/main.tf
+++ launch-template/main.tf
@@ -1,36 +1,4 @@
 *-------------------------------------------------------------------------------------------*
-// Create AWS security group
-resource "aws_security_group" "salary_sg" {
-  name        = var.security_group_name
-  description = var.security_group_description
-  vpc_id      = var.vpc_id
-
-  dynamic "ingress" {
-    for_each = var.ingress_rules
-    content {
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-    }
-  }
-
-  dynamic "egress" {
-    for_each = var.egress_rules
-    content {
-      from_port   = egress.value.from_port
-      to_port     = egress.value.to_port
-      protocol    = egress.value.protocol
-      cidr_blocks = egress.value.cidr_blocks
-    }
-  }
-
-  // Tags for the security group
-  // Tags for the security group
-   tags = var.Sg_tags
-}
-
-*-------------------------------------------------------------------------------------------*
 //Launch-Template
 resource "aws_launch_template" "salary_launch_template" {
   name_prefix   = var.launch_template_name_prefix
