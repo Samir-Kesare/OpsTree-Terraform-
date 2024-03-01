598667d502ada971e5794699c796ddd8160cf16c
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-03-01 10:15:09 +0530

    Update main.tf

--- Dev_Infra/Targetgroup/main.tf
+++ Dev_Infra/Targetgroup/main.tf
@@ -1,37 +1,3 @@
-*-------------------------------------------------------------------------------------------*
-// Create AWS security group
-resource "aws_security_group" "salary_sg" {
-  name        = var.security_group_name
-  description = var.security_group_description
-  vpc_id      = var.vpc_id
-
-  dynamic "ingress" {
-    for_each = var.ingress_rules
-    content {
-      from_port   = ingress.value.from_port
-      to_port     = ingress.value.to_port
-      protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_blocks
-    }
-  }
-
-  dynamic "egress" {
-    for_each = var.egress_rules
-    content {
-      from_port   = egress.value.from_port
-      to_port     = egress.value.to_port
-      protocol    = egress.value.protocol
-      cidr_blocks = egress.value.cidr_blocks
-    }
-  }
-
-  // Tags for the security group
-  Sg_tags                 = {
-    Name          = "salary-sg"
-    Enviroment    = "dev"
-    Owner         = "Shikha"
-  }   
-}
 
 *-------------------------------------------------------------------------------------------*
 //Launch-Template
