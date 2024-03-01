c877d33eb707eb21f3244f513010e6b512c8e949
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-01 09:22:16 +0530

    Create provider.tf

---
+++ Dev_Infra/Static_Tf/Attendance/provider.tf
@@ -0,0 +1,17 @@
+//PROVIDER
+
+// required terraform configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.38.0"  # Using a minimum version constraint
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  alias = "east"
+  region = "us-east-1"  
+}
