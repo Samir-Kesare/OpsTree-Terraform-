bd3fc3825bc7c2f240ff0d8b66416d1d82c2306d
Ubuntu <ubuntu@ip-172-31-83-89.ec2.internal>
2024-03-03 11:52:12 +0000

    Adding EC2 code for Redis Server

--- Dev_Infra/Static_Tf/redis/main.tf
+++ Dev_Infra/Static_Tf/redis/main.tf
@@ -1,4 +1,4 @@
-  
+
 # Security Group For Redis Servers #
 
 resource "aws_security_group" "redis_sg" {
@@ -28,4 +28,38 @@ resource "aws_security_group" "redis_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+/*------------Genrate-Key--------------*/
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+/*----------pem Key----------------------*/
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+/*----------Download Pem Key-------------------*/
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+/*-----------Redis Server-------------------*/
+
+resource "aws_instance" "redis_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.redis_server_type
+  key_name                    = aws_key_pair.key_pair.key_name
+  subnet_id                   = var.db_subnet_id
+  associate_public_ip_address = false
+  vpc_security_group_ids      = [aws_security_group.redis_sg.id]
+  tags = {
+    Name = var.redis_name
+    Type = "redis"
+  }
+
 }
