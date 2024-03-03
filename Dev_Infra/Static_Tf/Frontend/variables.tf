c30a7bb2de2b19f300b92950a560e97c4509469f
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-03 11:06:14 +0530

    Update variables.tf

--- Dev_Infra/Static_Tf/Frontend/variables.tf
+++ Dev_Infra/Static_Tf/Frontend/variables.tf
@@ -3,24 +3,25 @@
 variable "security_name" {
   description     = "Name tag for the security group"
   type            = string
-  default         = "Frontend-sg"
+  default         = "Dev-Frontend-sg"
 }
 variable "Security_description" {
   description     = "Description for the security group"
   type            = string
-  default         = "Security group for Frontend-API"
+  default         = "Security group for Dev-Frontend-API"
 }
-variable "vpc_id" {
+variable "SG_vpc_id" {
   description     = "ID of the VPC for instances"
   type            = string
-  default         = "vpc-0383f4dc3af051efa"
+  default         = "vpc-0383f4dc3af051efa"   # Dev-VPC ID
 }
 variable "inbound_ports" {
   description     = "List of inbound ports and protocols and cidr block"
   type            = list(map(any))
   default         = [
     { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, # Management VPC Cidr Block
-    { port = 3000, protocol = "tcp", cidr_blocks = "0.0.0.0/0" }, #  Frontend-lb-sg ID Repalce (0.0.0.0/0)
+    { port = 22, protocol = "tcp", security_group_ids = "sg-0f470a22a92136557" },    # OpenVPN-SG
+    { port = 3000, protocol = "tcp", security_group_ids = "sg-0b426399b2b19b0ae" }, #  Dev-Frontend-lb-sg ID  
   ]
 }
 variable "outbound_ports" {
@@ -34,25 +35,40 @@ variable "Sg_tags" {
   description     = "Tags for Security Group"
   type            = map(string)
   default         = {
-    Name          = "Frontend-sg"
+    Name          = "Dev-Frontend-sg"
     Enviroment    = "dev"
     Owner         = "Vishal"
   }
 }
+
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
 #--------------------------------Launch Template ----------------------------------#
 
 
 # AMI
+
 variable "AMI_name" {
   description     = "Give AMI Name"
   type            = string
-  default         = "Fronted-AMI" 
+  default         = "Dev-Fronted-AMI" 
 }
 variable "AMI_Instance_ID" {
-  description     = "Give Instance ID"
+  description     = "Give Dev-Frontend Instance ID"
   type            = string
-  default         = "i-01cbd03e43fabd05d"  
+  default         = "i-0572a5faad61b261e"  # Dev-Frontend Instance ID
+}
+
+# Key Generate
+
+variable "private_key_algorithm" {
+  description = "value"
+  type = string
+  default = "RSA"
+}
+variable "private_key_rsa_bits" {
+  description = "value"
+  type = number
+  default = 4096
 }
 
 # Launch Template 
@@ -60,12 +76,12 @@ variable "AMI_Instance_ID" {
 variable "template_name" {
   description     = "Launch Template Name"
   type            = string
-  default         = "Frontend-template"  
+  default         = "Dev-Frontend-template"  
 }
 variable "template_description" {
   description     = "Launch Template Description"
   type            = string
-  default         = "Template for Frontend"  
+  default         = "Template for Dev-Frontend"  
 }
 variable "instance_type" {
   description     = "Launch Template Instance Type"
@@ -75,7 +91,7 @@ variable "instance_type" {
 variable "instance_keypair" {
   description     = "Launch Template Instance Type keypair name"
   type            = string
-  default         = "newkey"  
+  default         = "Dev_Key"  
 }
 variable "subnet_ID" {
   description     = "Launch Template Subnet ID"
@@ -85,7 +101,3 @@ variable "subnet_ID" {
 }
 
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
-
-
-
-
