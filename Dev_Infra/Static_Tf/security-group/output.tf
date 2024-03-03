bf4c569a9e3f63fb206b8c1b35cd26d704d75505
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-03-03 16:24:35 +0530

    Update output.tf

--- Dev_Infra/Static_Tf/security-group/output.tf
+++ Dev_Infra/Static_Tf/security-group/output.tf
@@ -2,3 +2,15 @@
 output "security_group_id" {
   value = aws_security_group.salary_sg.id
 }
+*--------------------------------------------------------------------------------------------------------*
+utput "alb_arn" {
+  value = aws_lb.dev_alb.dns_name
+}
+
+output "dev_salary_tg_arn" {
+  value = aws_lb_target_group.dev_salary_tg.arn
+}
+
+output "listener_rule_priority" {
+  value = aws_lb_listener_rule.dev_listener_rule.priority
+}
