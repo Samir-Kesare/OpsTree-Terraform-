689da4d3951a1256e2db5e26b40156e608206a36
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-03 11:10:40 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/Frontend/main.tf
+++ Dev_Infra/Static_Tf/Frontend/main.tf
@@ -4,10 +4,10 @@ locals {
   inbound_ports         = var.inbound_ports
   outbound_ports        = var.outbound_ports
 }
-resource "aws_security_group" "sq_grp" {
+resource "aws_security_group" "security_group" {
   name                  = var.security_name
   description           = var.Security_description
-  vpc_id                = var.vpc_id
+  vpc_id                = var.SG_vpc_id
 
   dynamic "ingress" {
   for_each              = local.inbound_ports
@@ -15,7 +15,9 @@ resource "aws_security_group" "sq_grp" {
    from_port            = ingress.value.port
    to_port              = ingress.value.port
    protocol             = ingress.value.protocol
-   cidr_blocks          = [ingress.value.cidr_blocks]
+   # Conditionally apply CIDR block or security group rule based on type
+   cidr_blocks      = contains(keys(ingress.value), "cidr_blocks") ? [ingress.value.cidr_blocks] : null
+   security_groups  = contains(keys(ingress.value), "security_group_ids") ? [ingress.value.security_group_ids] : null
   }
  }
  dynamic "egress" {
@@ -40,29 +42,55 @@ resource "aws_ami_from_instance" "AMI" {
   source_instance_id = var.AMI_Instance_ID
 }
 
+# Generate Private Key
+resource "tls_private_key" "private_key" {
+  algorithm = var.private_key_algorithm
+  rsa_bits  = var.private_key_rsa_bits
+}
+
+# Generate SSH Key Pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.instance_keypair
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
+# Download Private Key in PEM Format
+resource "local_file" "private_key" {
+  content  = tls_private_key.private_key.private_key_pem
+  filename = "${var.instance_keypair}.pem"
+}
+
+
 # Cretae Launch Template
 
-# Launch Template Resource
-resource "aws_launch_template" "Template" {
-  name              = var.template_name
-  description       = var.template_description
-  image_id          = aws_ami_from_instance.AMI.id
-  instance_type     = var.instance_type
-
-  vpc_security_group_ids = [aws_security_group.sq_grp.id]
-  key_name               = var.instance_keypair
-  network_interfaces {
-    subnet_id            = var.subnet_ID 
+resource "null_resource" "configure_template" {
+  # Trigger the provisioner whenever the ALB DNS name changes
+  triggers = {
+    alb_dns_name = aws_lb.Dev_Alb.dns_name
   }
-  /*user_data = 
-                    #!/bin/bash
-                    cd /home/ubuntu/Frontend            # Uncomment When use Actual Instance
-                    npm start
-  */
-  tags = {
-    Name                  = var.template_name
+
+  # ALB DNS name replaced in script.sh
+  provisioner "local-exec" {
+    command = "sed -i 's/{{ALB_DNS}}/${aws_lb.Dev_Alb.dns_name}/g' ./script.sh"
   }
 }
 
-#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
+resource "aws_launch_template" "Template" {
+  name                      = var.template_name
+  description               = var.template_description
+  image_id                  = aws_ami_from_instance.AMI.id         # if your AMI alreday created then give your AMI ID
+  instance_type             = var.instance_type
+  key_name                  = aws_key_pair.key_pair.key_name
+  network_interfaces {
+    security_groups         = [aws_security_group.security_group.id]
+    subnet_id               = var.subnet_ID 
+  }
+  user_data = filebase64("./script.sh")
+  tags = {  
+    Name                  = var.template_name
+  }
+  depends_on = [ aws_lb.Dev_Alb, null_resource.configure_template ]
 
+}
+
+#-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
