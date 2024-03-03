191f02d398a49f025e2d7fc07b2851ff139f4697
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-03-03 15:50:02 +0530

    Delete launch-template directory

--- launch-template/output.tf
+++
@@ -1,6 +0,0 @@
-
-*------------------------------------------------------------------------------------------*
-//Launch-Template
-output "launch_template_id" {
-  value = aws_launch_template.salary_launch_template.id
-}
