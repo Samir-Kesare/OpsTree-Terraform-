6c8f3e57f3f72c79edcc6a768134f34c4d61c54a
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-04 11:17:21 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/Attendance/launch_template/main.tf
+++ Dev_Infra/Static_Tf/Attendance/launch_template/main.tf
@@ -1,3 +1,30 @@
+resource "aws_security_group" "attendance-sg" {
+  name        = var.security_group_name
+  description = var.description
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.inbound_rules
+    content {
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = ingress.value.protocol
+      cidr_blocks = [ingress.value.source]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.outbound_rules
+    content {
+      from_port       = egress.value.port
+      to_port         = egress.value.port
+      protocol        = egress.value.protocol
+      cidr_blocks     = [egress.value.source]
+    }
+  }
+  tags = var.sg_tags
+}
+
 resource "aws_launch_template" "Attendance_Launch_Template" {
   name          = var.template_name
   description   = var.template_description
