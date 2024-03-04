cc7dc4d9dec17eb4408c5619c3e4dd8afd534ab2
vyadavP7 <156056349+vyadavP7@users.noreply.github.com>
2024-03-04 17:34:17 +0530

    Update output.tf

--- Dev_Infra/Static_Tf/Attendance/launch_template/output.tf
+++ Dev_Infra/Static_Tf/Attendance/launch_template/output.tf
@@ -7,3 +7,16 @@ output "launch_template_id" {
   description = "The ID of the created launch template"
   value       = aws_launch_template.Attendance_Launch_Template.id
 }
+
+output "key_pair_name" {
+  description = "key name"
+  value       = [aws_key_pair.key_pair.key_name]
+}
+
+output "AMI_ID" {
+    value = [aws_ami_from_instance.AMI.id] 
+}
+
+output "Security_Group_ID" {
+  value = [aws_security_group.attendance-sg.id]
+}
