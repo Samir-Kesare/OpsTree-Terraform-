bd8aeab3ccc94ff372ce5637606fc32624b1d529
Vishalkk1998 <156056413+Vishalkk1998@users.noreply.github.com>
2024-03-05 14:14:59 +0530

    Update output.tf

--- Dev_Infra/Static_Tf/Frontend/output.tf
+++ Dev_Infra/Static_Tf/Frontend/output.tf
@@ -23,21 +23,10 @@ output "launch_template_id" {
   value = [aws_launch_template.Template.id]
 }
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
-#--------------------------------Launch Template ----------------------------------#
+#--------------------------------- Target Group -----------------------------------#
 
-# AMI
-output "AMI_ID" {
-    value = [aws_ami_from_instance.AMI.id] 
+output "Target_group_id" {
+  value = [aws_lb_target_group.Target_group.id]
 }
 
-# Priavte Key
-output "key_pair_name" {
-  description = "key name"
-  value       = [aws_key_pair.key_pair.key_name]
-}
-
-# Template
-output "launch_template_id" {
-  value = [aws_launch_template.Template.id]
-}
 #-----------------------xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -----------------------#
