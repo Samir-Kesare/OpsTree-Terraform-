31a7456f8affec97a8ba1b97963ef5cd983ea7af
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-03-05 12:30:21 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/security-group/main.tf
+++ Dev_Infra/Static_Tf/security-group/main.tf
@@ -27,3 +27,70 @@ resource "aws_security_group" "salary_sg" {
   // Tags for the security group
    tags                  = var.Sg_tags
 }
+*---------------------------------------------------------------------------------------------------------*
+// Create AWS AMI from Instance
+resource "aws_ami_from_instance" "AMI" {
+  name               = var.AMI_name
+  source_instance_id = var.AMI_Instance_ID
+}
+
+// Generate SSH Key Pair
+resource "tls_private_key" "private_key" {
+  algorithm = var.private_key_algorithm
+  rsa_bits  = var.private_key_rsa_bits
+}
+
+// Create AWS Key Pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.instance_keypair
+  public_key = tls_private_key.private_key.public_key_openssh
+}
+
+// Download Private Key in PEM Format
+resource "local_file" "private_key" {
+  content  = tls_private_key.private_key.private_key_pem
+  filename = "${var.instance_keypair}.pem"
+}
+
+// Create AWS Launch Template
+resource "aws_launch_template" "Salary_Launch_Template" {
+  name          = var.template_name
+  description   = var.template_description
+  image_id      = aws_ami_from_instance.AMI.id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.key_pair.key_name
+
+  network_interfaces {
+    security_groups = [aws_security_group.salary_sg.id]
+    subnet_id       = var.subnet_ID
+  }
+
+  user_data = filebase64("./script.sh")
+  
+  tags = {
+    Name = var.template_name
+  }
+}
+
+// Target groups 
+
+resource "aws_lb_target_group" "Target_group" {
+  name        = var.target_group_name
+  port        = var.target_group_port
+  protocol    = var.target_group_protocol
+  vpc_id      = var.TG_vpc_id
+
+  health_check {
+    path                = var.health_check_path
+    protocol            = var.target_group_protocol
+    port                = var.health_check_port
+    interval            = var.health_check_interval
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+  }
+
+  tags = {
+    Name = var.target_group_name
+  }
+}
