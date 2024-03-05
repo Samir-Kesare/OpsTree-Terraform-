155cb402d700ddb225ba7cd8c0e12c1b30a5f229
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-03-05 18:57:27 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/security-group/main.tf
+++ Dev_Infra/Static_Tf/security-group/main.tf
@@ -28,176 +28,99 @@ resource "aws_security_group" "salary_sg" {
    tags                  = var.Sg_tags
 }
 *---------------------------------------------------------------------------------------------------------*
-// launch template
-variable "template_name" {
-  description = "The name of the launch template"
-  type        = string
-  default     = "Salary API-Launch-Template"
+// Create AWS AMI from Instance
+resource "aws_ami_from_instance" "AMI" {
+  name               = var.AMI_name
+  source_instance_id = var.AMI_Instance_ID
 }
 
-variable "template_description" {
-  description = "description of launch template"
-  type        = string
-  default     = "Launch template for salary api"
+// Generate SSH Key Pair
+resource "tls_private_key" "private_key" {
+  algorithm = var.private_key_algorithm
+  rsa_bits  = var.private_key_rsa_bits
 }
 
-variable "instance_type" {
-  description = "The type of instance to launch"
-  type        = string
-  default     = "t2.micro"
+// Create AWS Key Pair
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.instance_keypair
+  public_key = tls_private_key.private_key.public_key_openssh
 }
 
-variable "subnet_ID" {
-  description = "The ID of the subnet"
-  type        = string
-  default     = "subnet-013843b2702f341f4"
+// Download Private Key in PEM Format
+resource "local_file" "private_key" {
+  content  = tls_private_key.private_key.private_key_pem
+  filename = "${var.instance_keypair}.pem"
 }
 
+// Create AWS Launch Template
+resource "aws_launch_template" "Salary_Launch_Template" {
+  name          = var.template_name
+  description   = var.template_description
+  image_id      = aws_ami_from_instance.AMI.id
+  instance_type = var.instance_type
+  key_name      = aws_key_pair.key_pair.key_name
 
-// AMI
+  network_interfaces {
+    security_groups = [aws_security_group.salary_sg.id]
+    subnet_id       = var.subnet_ID
+  }
 
-variable "AMI_name" {
-  description     = " AMI Name"
-  type            = string
-  default         = "Dev-Salary-AMI" 
-}
-variable "AMI_Instance_ID" {
-  description     = " Dev-Salary Instance ID"
-  type            = string
-  default         = "i-069d5031c2f20d32b"  # Instance ID of Salary-API
-}
-
-// Generate Key
-
-variable "private_key_algorithm" {
-  description = "value"
-  type = string
-  default = "RSA"
-}
-variable "private_key_rsa_bits" {
-  description = "value"
-  type = number
-  default = 4096
-}
-
-variable "instance_keypair" {
-  description     = "Launch Template Instance Type keypair name"
-  type            = string
-  default         = "Dev_Key"  
+  user_data = filebase64("./script.sh")
+  
+  tags = {
+    Name = var.template_name
+  }
 }
 
 // Target groups 
 
-variable "target_group_name" {
-  description = "The name of the target group"
-  type        = string
-  default         = "Dev-Salary-TG"
-}
+resource "aws_lb_target_group" "Target_group" {
+  name        = var.target_group_name
+  port        = var.target_group_port
+  protocol    = var.target_group_protocol
+  vpc_id      = var.TG_vpc_id
 
-variable "target_group_port" {
-  description = "The port on which targets receive traffic"
-  type        = number
-  default         = 80
-}
+  health_check {
+    path                = var.health_check_path
+    protocol            = var.target_group_protocol
+    port                = var.health_check_port
+    interval            = var.health_check_interval
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+  }
 
-variable "target_group_protocol" {
-  description = "The protocol to use for routing traffic to the targets"
-  type        = string
-  default         = "HTTP"
-}
-
-variable "TG_vpc_id" {
-  description = "The VPC ID"
-  type        = string
-  default         = "vpc-0ebc6865d6c6a5460" // dev vpc id
-}
-
-variable "health_check_path" {
-  description = "The destination for the health check request"
-  type        = string
-  default         = "/api/v1/salary/health"
-}
-
-variable "health_check_port" {
-  description = "The port to use to connect with the target"
-  type        = string
-  default         = "traffic-port"
-}
-
-variable "health_check_interval" {
-  description = "The approximate amount of time, in seconds, between health checks of an individual target"
-  type        = number
-  default         = 30
-}
-
-variable "health_check_timeout" {
-  description = "The amount of time, in seconds, during which no response from a target means a failed health check"
-  type        = number
-  default         = 5
-}
-
-variable "health_check_healthy_threshold" {
-  description = "The number of consecutive health checks successes required before considering an unhealthy target healthy"
-  type        = number
-  default     = 5
-}
-variable "health_check_unhealthy_threshold" {
-  description = "The number of consecutive health check failures required before considering a target unhealthy"
-  type        = number
-  default     = 2
+  tags = {
+    Name = var.target_group_name
+  }
 }
 
 # Configure ALB
 
-variable "alb_name" {
-  description     = "Name for the Application Load Balancer"
-  type            = string
-  default         = "Dev-ALB"
+resource "aws_lb" "Dev_Alb" {
+  name               = var.alb_name
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  security_groups    = var.security_groups
+  subnets            = var.subnets
+  tags = {
+    Name = var.alb_name
+  }
 }
-variable "internal" {
-  description     = "Boolean flag indicating whether the ALB is internal or external"
-  type            = bool
-  default         = false
-}
-variable "load_balancer_type" {
-  description     = "Type of the load balancer"
-  type            = string
-  default         = "application"
-}
-variable "security_groups" {
-  description     = "List of security group IDs for the ALB"
-  type            = list(string)
-  default         = ["sg-09132d5dee9e5e106"]  # Salary-lb-sg ID
-}
-variable "subnets" {
-  description     = "List of subnet IDs for the ALB"
-  type            = list(string)
-  default         = ["subnet-0a2270e6f508e903d", "subnet-06a5a25b82ec957cf"]  # Public subnet IDs 
-}
+// Create listener rule for salary
 
-
-// Listener rule
-
-variable "listener_arn" {
-  description = "ARN of the existing listener where the rule will be added"
-  type        = string
-  default = "arn:aws:elasticloadbalancing:us-east-1:975050171850:listener/app/ALB/49e9e7b843170b35/85a562da2e108bf6"
-}
-
-variable "path_pattern" {
-  description = "Path pattern for the listener rule"
-  type        = string
-  default     = "/api/v1/salary/*"
-}
-
-variable "action_type" {
-  description = "Path pattern for the listener rule"
-  type        = string
-  default     = "forward"
-}
-
-variable "target_group_arn" {
-  description = "ARN of the target group"
-  type        = string
-  default     = "arn:aws:elasticloadbalancing:us-east-1:975050171850:targetgroup/salaryapi/8f778507e433b5f1"
+resource "aws_lb_listener_rule" "path_rule" {
+  listener_arn = var.listener_arn
+  priority     = 100
+  
+  action {
+    type             = var.action_type
+    target_group_arn = var.target_group_arn
+  }
+  
+  condition {
+    path_pattern {
+      values = [var.path_pattern]
+    }
+  }
 }
