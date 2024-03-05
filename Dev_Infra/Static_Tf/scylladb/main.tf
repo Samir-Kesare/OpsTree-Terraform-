12873402953a8f4cb6b5ec5b583c88ac30d27a19
Ubuntu <ubuntu@ip-172-31-83-89.ec2.internal>
2024-03-05 06:12:48 +0000

    Adding Scylla DB Server

--- Dev_Infra/Static_Tf/scylladb/main.tf
+++ Dev_Infra/Static_Tf/scylladb/main.tf
@@ -28,3 +28,37 @@ resource "aws_security_group" "scylladb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+/*------------Genrate-Key--------------*/
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+/*----------pem Key----------------------*/
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+/*----------Download Pem Key-------------------*/
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
+
+/*-----------Scylla DB Server-------------------*/
+
+resource "aws_instance" "scylladb_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.scylladb_server_type
+  key_name                    = aws_key_pair.key_pair.key_name
+  subnet_id                   = var.db_subnet_id
+  associate_public_ip_address = false
+  vpc_security_group_ids      = [aws_security_group.scylladb_sg.id]
+  tags = {
+    Name = var.scylladb_name
+    Type = "scylladb"
+  }
+
+}
