84499cf46a125a47ff91ce4183134d808cb8f46b
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-03-06 15:54:21 +0530

    Update output.tf

--- Dev_Infra/EC2/output.tf
+++ Dev_Infra/EC2/output.tf
@@ -1,3 +1,8 @@
-output "instance_public_ip" {
-  value = aws_instance.Postgres_Instance.public_ip
+
+output "Postgres_Instance_id" {
+  value = [aws_instance.Postgres_server.id]
+}
+
+output "Postgresql_sg_id" {
+  value = aws_security_group.Postgresql_sg.id
 }
