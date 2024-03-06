7f6908dc45d3bf1b17bbe8bd9407a369baee2704
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-03-06 15:53:55 +0530

    Update main.tf

--- Dev_Infra/EC2/main.tf
+++ Dev_Infra/EC2/main.tf
@@ -1,73 +1,74 @@
-
 #---------------------------------Security Group ----------------------------------#
 
-locals {
-  inbound_ports         = var.inbound_ports
-  outbound_ports        = var.outbound_ports
-}
-resource "aws_security_group" "Security_group" {
-  name                  = var.security_name
-  description           = var.Security_description
-  vpc_id                = var.vpc_id
+
+
+resource "aws_security_group" "Postgresql_sg" {
+  name        = "Postgresql_sg"
+  description = "Security group for Postgresql DB Servers"
+
+  vpc_id = var.vpc_id
 
   dynamic "ingress" {
-  for_each              = local.inbound_ports
-  content {
-   from_port            = ingress.value.port
-   to_port              = ingress.value.port
-   protocol             = ingress.value.protocol
-   cidr_blocks          = [ingress.value.cidr_blocks]
+    for_each = {
+      "ssh_management" = var.management_vpc_cidr
+      "ssh_openvpn"    = var.openvpn_sg_id
+      "Postgres_backend"  = var.backend_sg_id
+      
+    }
+    content {
+      from_port       = ingress.value == var.management_vpc_cidr ? var.ssh_port : ingress.value == var.openvpn_sg_id ? 22 : var.Postgres_port
+      to_port         = ingress.value == var.management_vpc_cidr ? var.ssh_port : ingress.value == var.openvpn_sg_id ? 22 : var.Postgres_port
+      protocol        = ingress.value == var.management_vpc_cidr ? "tcp" : ingress.value == var.openvpn_sg_id ? "tcp" : "tcp"
+      cidr_blocks     = ingress.value == var.management_vpc_cidr ? [ingress.value] : null
+      security_groups = ingress.value == var.openvpn_sg_id ? [ingress.value] : ingress.value == var.backend_sg_id ? [ingress.value] : null
+    }
   }
- }
- dynamic "egress" {
-  for_each              = local.outbound_ports
-  content {
-   from_port            = egress.value.port
-   to_port              = egress.value.port
-   protocol             = egress.value.protocol
-   cidr_blocks          = [egress.value.cidr_blocks]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
- }
-  tags                  = var.Security_group_tags
 }
 
 
 
 #---------------------------------EC2 Instance ----------------------------------#
 
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
 
-  tags = {
-    Name = var.vpc_name
-  }
+
+
+/*------------Genrate-Key--------------*/
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr_block
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = var.private_subnet_name
-  }
+/*----------pem Key----------------------*/
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-resource "aws_instance" "Postgres_Instance" {
-  ami             = var.instance_ami
-  instance_type   = var.instance_type
-  key_name        = var.key_name
-  security_groups = [var.security_name]
-  
+/*----------Download Pem Key-------------------*/
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
+}
 
-  subnet_id = aws_subnet.private_subnet.id  
+/*-----------Postgres Server-------------------*/
 
-  root_block_device {
-    volume_size = 16
-  }
-
-  tags = var.Postgres_Instance_tag
-
-  # Disable public IP address assignment
+resource "aws_instance" "Postgres_server" {
+  ami                         = var.ami_id
+  instance_type               = var.Postgres_server_type
+  key_name                    = var.key_name
+  subnet_id                   = var.db_subnet_id
   associate_public_ip_address = false
+  vpc_security_group_ids      = [aws_security_group.Postgresql_sg.id]
+  tags = {
+    Name = var.Postgres_name
+    Type = "Postgres"
+  }
+
 }
