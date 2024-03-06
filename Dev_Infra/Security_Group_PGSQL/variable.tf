08a66028fbc216a04c354a985e418e78189d6268
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-03-06 13:50:37 +0530

    Update variable.tf

--- Dev_Infra/Security_Group_PGSQL/variable.tf
+++ Dev_Infra/Security_Group_PGSQL/variable.tf
@@ -1,43 +1,29 @@
 
 #---------------------------------Security Group ----------------------------------#
 
-variable "security_name" {
-  description     = "Name tag for the security group"
-  type            = string
-  default         = "Postgres-sg"
-}
-variable "Security_description" {
-  description     = "Description for the security group"
-  type            = string
-  default         = "Security group for Postgres"
-}
 variable "vpc_id" {
-  description     = "ID of the VPC for instances"
-  type            = string
-  default         = "vpc-0fd6d3005998fb58c"
+  description = " VPC id of the Development VPC"
+  type        = string
 }
-variable "inbound_ports" {
-  description     = "List of inbound ports and protocols and cidr block"
-  type            = list(map(any))
-  default         = [
-    { port = 22, protocol = "tcp",cidr_blocks = "20.0.0.0/28" }, # Management VPC Cidr Block
-    { port = 5432, protocol = "tcp", cidr_blocks = "0.0.0.0/0" }, #  Backend-sg
-  ]
+variable "backend_sg_id" {
+  description = "SG id of the backend servers"
+  type        = string
 }
-
-variable "outbound_ports" {
-  description     = "List of outbound ports and protocols and Cidr block "
-  type            = list(map(any))
-  default         = [
-    { port = 0, protocol = "-1", cidr_blocks = "0.0.0.0/0", },
-  ]
+variable "openvpn_sg_id" {
+  description = "SG id of OpenVPN server"
+  type        = string
 }
-variable "Security_group_tags" {
-  type            = map(string)
-  default         = {
-    Name          = "Postgres-SG"
-    Enviroment    = "dev"
-    Owner         = "Vikram"
-  }
+variable "management_vpc_cidr" {
+  description = "Management VPC Subnet CIDR"
+  type        = string
 }
-
+variable "Postgres_port" {
+  description = "Default scylladb port"
+  type        = string
+  default     = "5432"
+}
+variable "ssh_port" {
+  description = "Default SSH port"
+  type        = string
+  default     = "22"
+}
