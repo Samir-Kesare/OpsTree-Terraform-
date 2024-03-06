85369eeea14edbc01c5d8aba3d739089928d13ac
code-shantanu <156056364+code-shantanu@users.noreply.github.com>
2024-03-06 19:27:35 +0530

    Update variables.tf

--- Dev_Infra/Static_Tf/Employee/variables.tf
+++ Dev_Infra/Static_Tf/Employee/variables.tf
@@ -40,3 +40,62 @@ variable "Sg_tags" {
     Owner         = "Shantanu"
   }
 }
+
+
+#--------------------------------Launch Template ----------------------------------#
+
+
+# AMI
+
+variable "AMI_name" {
+  description     = "Give AMI Name"
+  type            = string
+  default         = "Dev-Employee-AMI" 
+}
+variable "AMI_Instance_ID" {
+  description     = "Give Dev-Employee Instance ID"
+  type            = string
+  default         = "i-0e1e08aa641b7ed67"  # Dev-employee Instance ID
+}
+
+# Key Generate
+
+variable "private_key_algorithm" {
+  description = "value"
+  type = string
+  default = "RSA"
+}
+variable "private_key_rsa_bits" {
+  description = "value"
+  type = number
+  default = 4096
+}
+
+# Launch Template 
+
+variable "template_name" {
+  description     = "Launch Template Name"
+  type            = string
+  default         = "Dev-Employee-template"  
+}
+variable "template_description" {
+  description     = "Launch Template Description"
+  type            = string
+  default         = "Template for Dev-Employee"  
+}
+variable "instance_type" {
+  description     = "Launch Template Instance Type"
+  type            = string
+  default         = "t2.medium"  
+}
+variable "instance_keypair" {
+  description     = "Launch Template Instance Type keypair name"
+  type            = string
+  default         = "Dev_Key"  
+}
+variable "subnet_ID" {
+  description     = "Launch Template Subnet ID"
+  type            = string
+  default         = "subnet-029ac81220960564f"  
+  
+}
