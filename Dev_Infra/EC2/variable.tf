c388dc735b6acd903bd8ec3434774e5965dd30d4
vikram445 <79625874+vikram445@users.noreply.github.com>
2024-03-06 15:56:12 +0530

    Update variable.tf

--- Dev_Infra/EC2/variable.tf
+++ Dev_Infra/EC2/variable.tf
@@ -1,95 +1,59 @@
 
-#---------------------------------Security Group ----------------------------------#
-
-variable "security_name" {
-  description     = "Name tag for the security group"
-  type            = string
-  default         = "Postgres_sg"
-}
-variable "Security_description" {
-  description     = "Description for the security group"
-  type            = string
-  default         = "Security group for Postgres"
-}
-
-variable "inbound_ports" {
-  description     = "List of inbound ports and protocols and cidr block"
-  type            = list(map(any))
-  
-}
-
-variable "outbound_ports" {
-  description     = "List of outbound ports and protocols and Cidr block "
-  type            = list(map(any))
- 
-}
-variable "Security_group_tags" {
-  type            = map(string)
-  default         = {
-    Name          = "Postgres-SG"
-    Enviroment    = "dev"
-    Owner         = "Vikram"
-  }
-}
-
-
-
-
-#---------------------------------EC2 Instance ----------------------------------#
-
-
-
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources"
-}
-
-variable "instance_ami" {
-  description = "The AMI for the EC2 instance"
-}
-
-variable "instance_type" {
-  description = "The type of EC2 instance"
-}
-
-variable "key_name" {
-  description = "The SSH key pair name to use for the instance"
-}
-
-variable "vpc_name" {
-  description = "Name for the VPC"
-  type        = string
-}
-
+# #---------------------------------Security Group ----------------------------------#
 
 variable "vpc_id" {
-  description     = "ID of the VPC for instances"
-  type            = string
+  description = " VPC id of the Development VPC"
+  type        = string
+}
+variable "backend_sg_id" {
+  description = "SG id of the backend servers"
+  type        = string
+}
+variable "openvpn_sg_id" {
+  description = "SG id of OpenVPN server"
+  type        = string
+}
+variable "management_vpc_cidr" {
+  description = "Management VPC Subnet CIDR"
+  type        = string
+}
+variable "Postgres_port" {
+  description = "Default scylladb port"
+  type        = string
+  default     = "5432"
+}
+variable "ssh_port" {
+  description = "Default SSH port"
+  type        = string
+  default     = "22"
 }
 
-variable "private_subnet_cidr_block" {
-  description = "CIDR block for the private subnet"
+
+
+
+# #---------------------------------EC2 Instance ----------------------------------#
+
+variable "key_name" {
+  description = "Name of the SSH key pair"
   type        = string
 }
 
-variable "availability_zone" {
-  description = "Availability zone for the private subnet"
+variable "Postgres_server_type" {
+  description = "Instance type for Redis server"
   type        = string
 }
 
-variable "private_subnet_name" {
-  description = "Name for the private subnet"
+variable "db_subnet_id" {
+  description = "ID of the subnet for the Redis server"
   type        = string
 }
 
-
-variable "Postgres_Instance_tag" {
-  type            = map(string)
-  
-}
-
-variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
+variable "Postgres_name" {
+  description = "Name tag for the Redis server"
   type        = string
 }
 
+variable "ami_id" {
+  description = "AMI ID for your instance"
+  type        = string
+}
