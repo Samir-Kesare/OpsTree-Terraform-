3f037e08d9257ee8105f47344e2e2151e0da2de5
code-shantanu <156056364+code-shantanu@users.noreply.github.com>
2024-03-09 19:17:01 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/Employee/main.tf
+++ Dev_Infra/Static_Tf/Employee/main.tf
@@ -76,3 +76,35 @@ resource "aws_launch_template" "Template" {
     Name                  = var.template_name
   }
 }
+
+#--------------------------------- Target Group -----------------------------------#
+
+resource "aws_lb_target_group" "Target_group" {
+  name        = var.target_group_name
+  port        = var.target_group_port
+  protocol    = var.target_group_protocol
+  vpc_id      = var.TG_vpc_id
+
+  health_check {
+    path                = var.health_check_path
+    protocol            = var.target_group_protocol
+    port                = var.health_check_port
+    interval            = var.health_check_interval
+    timeout             = var.health_check_timeout
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+  }
+
+  tags = {
+    Name = var.target_group_name
+  }
+}
+
+# Attach instances to the target group
+
+# resource "aws_lb_target_group_attachment" "Target_group_attachment" {
+#   for_each         = toset(var.instance_ids)
+#   target_group_arn = aws_lb_target_group.Target_group.arn
+#   target_id        = each.value
+#   port             = var.target_group_port
+# }
