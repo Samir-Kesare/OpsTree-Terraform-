d252ae52ac7e9cbd6a9b0c19600a0d39bd492d72
code-shantanu <156056364+code-shantanu@users.noreply.github.com>
2024-03-09 22:51:32 +0530

    Update variables.tf

--- Dev_Infra/Static_Tf/Employee/variables.tf
+++ Dev_Infra/Static_Tf/Employee/variables.tf
@@ -210,3 +210,53 @@ variable "target_group_arn" {
   type        = string
   default     = "arn:aws:elasticloadbalancing:ap-southeast-1:441247711986:targetgroup/Dev-Employee-TG/efbd475456f84803"
 }
+
+#--------------------------Configure Auto Scaling group ---------------------------#
+
+variable "autoscaling_group_name" {
+  description     = "The name of the Auto Scaling Group"
+  type            = string
+  default         = "Dev_Employee_ASG"
+}
+
+variable "min_size" {
+  description     = "The minimum number of instances in the ASG"
+  type            = number
+  default         = 1
+}
+
+variable "max_size" {
+  description     = "The maximum number of instances in the ASG"
+  type            = number
+  default         = 2
+}
+
+variable "desired_capacity" {
+  description     = "The desired number of instances in the ASG"
+  type            = number
+  default         = 1
+}
+
+variable "subnet_ids" {
+  description     = "The list of subnet IDs where the instances will be launched"
+  type            = list(string)
+  default         = [ "subnet-029ac81220960564f" ]    #Employee-Pvt-Subnet ID
+}
+
+variable "tag_key" {
+  description     = "The key for the tag to be applied to the ASG and instances"
+  type            = string
+  default         = "Name"
+}
+
+variable "tag_value" {
+  description     = "The value for the tag to be applied to the ASG and instances"
+  type            = string
+  default         = "Dev_Employee_ASG"
+}
+
+variable "propagate_at_launch" {
+  description     = "Whether the tag should be propagated to instances launched by the ASG"
+  type            = bool
+  default         = true
+}
