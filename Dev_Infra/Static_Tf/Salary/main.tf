547baf5f2fa7f7d249a6eb816a4360e652e7490c
Harshit Singh <156056444+Panu-S-Harshit-Ninja-07@users.noreply.github.com>
2024-03-10 12:31:11 +0530

    Update main.tf

--- Dev_Infra/Static_Tf/Salary/main.tf
+++ Dev_Infra/Static_Tf/Salary/main.tf
@@ -115,7 +115,7 @@ resource "aws_lb_listener_rule" "path_rule" {
   
   action {
     type             = var.action_type
-    target_group_arn = var.target_group_arn
+    target_group_arn = aws_lb_target_group.Target_group.arn
   }
   
   condition {
