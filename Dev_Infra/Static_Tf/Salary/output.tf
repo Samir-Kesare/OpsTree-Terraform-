49a983c51632c9bb7388ce6e73a1da19ec0453b1
tripathishikha1 <156056746+tripathishikha1@users.noreply.github.com>
2024-03-10 06:09:44 +0530

    Update output.tf

--- Dev_Infra/Static_Tf/Salary/output.tf
+++ Dev_Infra/Static_Tf/Salary/output.tf
@@ -1,8 +1,9 @@
 // Output for the created AWS security group
 output "security_group_id" {
-  value = aws_security_group.salary_sg.id
+  value = aws_security_group.salary-sg.id
 }
 
+// Launch Template
 output "launch_template_id" {
   description = "The ID of the created launch template"
   value       = aws_launch_template.Salary_Launch_Template.id
@@ -31,7 +32,7 @@ output "ALB_arn" {
   value = [aws_lb.Dev_Alb.arn] 
 }
 output "load_balancer_dns_name" {
-  value = [aws_lb.Dev_Alb]
+  value = [aws_lb.Dev_Alb.dns_name]
 }
 
 // ASG
@@ -39,13 +40,9 @@ output "Autoscaling_group_id" {
   value = [aws_autoscaling_group.Salary_asg.id]
 }
 
+
 // ASG POLICY
 
 output "Autoscaling_policy_name" {
   value  = [aws_autoscaling_policy.Salary_ASG_Policy.name]
 }
-
-output "Target_group_id" {
-  value = [aws_lb_target_group.Target_group.id]
-}
-
